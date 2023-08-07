e1cd16f5677df1a7d4ed672ce4dfb59835792578
my portfolio <134843704+mggkgs@users.noreply.github.com>
2023-08-07 11:19:06 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project 1 - Expressions and Alert</title>
+</head>
+<body>
+    <script>
+        // Write the JavaScript code here
+        alert("Hello from Project 1!");
+    </script>
+</body>
+</html>
