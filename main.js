1aa1d66992d58e60dafb88a82eccb7f757ce23e7
my portfolio <134843704+mggkgs@users.noreply.github.com>
2023-08-08 21:40:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+{
+    Document.write("Lisa told Bart, \"Knok it off, Bart! or I will tell dad!\"<br>\"Eat my shorts!\ Bart responds.")
+
+var A = "Lisa told Bart, \"Knok it off, Bart! or I will tell dad!\"<br>\"Eat my shorts!\ Bart responds.";
+}
+
+
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
